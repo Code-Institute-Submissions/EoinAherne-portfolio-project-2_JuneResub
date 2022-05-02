fc2c760f1386a8e3e5735e8f9beaabf4d338e1a8
EoinAherne <aherneeoin88@gmail.com>
2022-05-02 11:51:00 +0000

    Added Javascript to get elements from the dom, added event listeners for buttons

---
+++ assets/gameScript.js
@@ -0,0 +1,15 @@
+//Get user and computer scores 
+let userScore = document.getElementById('user-score');
+let compScore = document.getElementById('computer-score');
+//Get game button choices 
+let rock = document.getElementById('rock');
+let paper = document.getElementById('paper');
+let scissors = document.getElementById('scissors');
+//Get result display message
+let resultDisplay = document.getElementById('result-display')
+
+//Add event listeners for buttons
+rock.addEventListener('click,'alert('rock clicked')  );   //add function
+paper.addEventListener('click,' );   //add function
+scissors.addEventListener('click,' ); //add function
+
