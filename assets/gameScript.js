acf2a030c30e65d0229ff74dfffb2703d7a301ac
EoinAherne <aherneeoin88@gmail.com>
2022-05-03 13:32:58 +0000

    Add play functions

--- assets/gameScript.js
+++ assets/gameScript.js
@@ -1,6 +1,5 @@
-//Get user and computer scores 
-let userScore = document.getElementById('user-score');
-let compScore = document.getElementById('computer-score');
+var userScore = 0;
+var compScore = 0;
 //Get game button choices 
 let rock = document.getElementById('rock');
 let paper = document.getElementById('paper');
@@ -14,11 +13,41 @@ let buttons = document.getElementsByClassName('.btn')
 /**Generate random number for getting computer input */ 
 function generateComputerChoice(event) {
     let randomNumber = Math.floor(Math.random() * 3) + 1;
-    console.log(randomNumber);
+    console.log("Computer Chooses:", randomNumber);
+    return randomNumber;
+    randomNumber = cpuchoice;
 }
 
 /**  create play function */
 function play(choice) {
     let cpuchoice = generateComputerChoice();
-    console.log(choice);
+    let text;
+//Testing the results with else if statements//   
+    if (choice == '1' && cpuchoice == '2'){
+        compScore +1;
+        text = 'Computer chooses Paper. Computer Wins';
+    } else if (choice =='2' && cpuchoice == '1'){
+        userScore +1;
+        text = 'Computer chooses Rock. User wins';
+    } else if (choice =='3' && cpuchoice == '1'){
+        compScore += 1;
+        text = 'Computer chooses Rock. Computer Wins';
+    } else if (choice == '1' && cpuchoice == '3'){   
+        userScore += 1;
+        text = 'Computer chooses Rock. User Wins';
+    } else if (choice == '2' && cpuchoice == '3'){ 
+        compScore += 1;
+        text = 'Computer chooses scissors. Computer Wins';
+    } else if (choice == '3' && cpuchoice == '2'){
+        userScore +=1;
+        text = 'Computer chooses paper. User Wins';
+    } else if (choice == cpuchoice) {      
+        text = 'Draw! Play again';
+    }
+
+        document.getElementById('result-display').innerHTML = text;
+        document.getElementById('user-score').innerHTML = userScore;
+        document.getElementById('computer-score').innerHTML = compScore;  
+
+        
 }
