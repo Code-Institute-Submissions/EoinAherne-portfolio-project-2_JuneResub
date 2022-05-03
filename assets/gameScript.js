208a3f907ecb7f9a4d280e3f93ee6b5e91a0309c
EoinAherne <aherneeoin88@gmail.com>
2022-05-03 15:33:06 +0000

    add reset button and declare function

--- assets/gameScript.js
+++ assets/gameScript.js
@@ -2,12 +2,7 @@
 //Open variables containing 0 to be ammended with results later 
 var userScore = 0;
 var compScore = 0;
-//Get game button choices 
-let rock = document.getElementById('rock');
-let paper = document.getElementById('paper');
-let scissors = document.getElementById('scissors');
-//Get result display message
-let resultDisplay = document.getElementById('result-display')
+
 // Get buttons as an array
 let buttons = document.getElementsByClassName('.btn') 
 
@@ -23,35 +18,41 @@ function generateComputerChoice(event) {
 /**  create play function */
 function play(choice) {
     let cpuchoice = generateComputerChoice();
-    let text;
+    
 //Testing the results with else if statements//   
+// rock = 1, paper = 2, scissors = 3 //
     if (choice == '1' && cpuchoice == '2'){
-        compScore +=1;
+        ++compScore;  
         text = 'Computer chooses Paper. Computer Wins';
     } else if (choice =='2' && cpuchoice == '1'){
-        userScore +=1;
+        ++userScore;
         text = 'Computer chooses Rock. User wins';
     } else if (choice =='3' && cpuchoice == '1'){
-        compScore += 1;
+        ++compScore;
         text = 'Computer chooses Rock. Computer Wins';
     } else if (choice == '1' && cpuchoice == '3'){   
-        userScore += 1;
+        ++userScore;
         text = 'Computer chooses Rock. User Wins';
     } else if (choice == '2' && cpuchoice == '3'){ 
-        compScore += 1;
+        ++compScore;
         text = 'Computer chooses scissors. Computer Wins';
     } else if (choice == '3' && cpuchoice == '2'){
-        userScore +=1;
+        ++userScore;
         text = 'Computer chooses paper. User Wins';
     } else if (choice == cpuchoice) {      
         text = 'Draw! Play again';
     } else { 
         text = "Computer scores. Take your next pick";
-        compScore+=1;
+        ++compScore;
     }
 
-document.getElementById('result-display').innerHTML = text;
+/** Get elements and update HTML in the Dom */    
+document.getElementById('result-display').innerHTML = text; 
 document.getElementById('user-score').innerHTML = userScore;
 document.getElementById('computer-score').innerHTML = compScore;  
     
 }
+
+function resetGame() {
+      
+    }
